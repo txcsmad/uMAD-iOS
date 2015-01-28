2b407c2490bf719dbdcc876d8eda2c977bfa1706
schun <s.chun@utexas.edu>
2015-01-27 21:39:27 -0600

    Integrated parse with events

---
+++ uMAD/Event.swift
@@ -0,0 +1,36 @@
+//
+//  Event.swift
+//  uMAD
+//
+//  Created by Andrew Chun on 1/26/15.
+//  Copyright (c) 2015 com.MAD. All rights reserved.
+//
+
+import Foundation
+
+class Event {
+    var companyName:    String!
+    var room:           String!
+    var image:          PFFile!
+    var speaker:        String!
+    var description:    String!
+    var startTime:      NSDate!
+    var endTime:        NSDate!
+    var email:          String!
+    var companyWebsite: NSURL!
+    var twitterHandle:  String!
+    
+    init(info: Dictionary<String, AnyObject>) {
+        companyName = info["companyName"]       as String!
+        room = info["room"]                     as String!
+        image = info["image"]                   as PFFile!
+        speaker = info["speaker"]               as String!
+        description = info["description"]       as String!
+        startTime = info["startTime"]           as NSDate!
+        endTime = info["endTime"]               as NSDate!
+        email = info["email"]                   as String!
+        companyWebsite = info["companyWebsite"] as NSURL!
+        twitterHandle = info["twitterHandle"]   as String!
+        
+    }
+}
--- uMAD/EventsViewController.swift
+++ uMAD/EventsViewController.swift
@@ -9,19 +9,89 @@
 import Foundation
 import UIKit
 
-class EventsViewController: UIViewController {
-    var tableView: UITableView!
-    
+class EventsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    private var tableView: UITableView!
+    private var events: [Event] = [Event]()
     
     override init() {
         super.init()
         
-        tableView = UITableView(frame: CGRectMake(0, 0, CGRectGetWidth(view.bounds), CGRectGetHeight(view.bounds)), style: UITableViewStyle.Grouped)
+        view.backgroundColor = UIColor.lightGrayColor()
+        
+        tableView = UITableView(frame: CGRectMake(0, 20.00, CGRectGetWidth(view.bounds), CGRectGetHeight(view.bounds)), style: .Plain)
+        tableView.delegate = self
+        tableView.dataSource = self
         view.addSubview(tableView)
+        
+        var query = PFQuery(className:"Events")
+        query.findObjectsInBackgroundWithBlock {
+            (objects: [AnyObject]!, error: NSError!) -> Void in
+            if error == nil {
+                // The find succeeded.
+                println("Successfully retrieved \(objects.count) events.")
+                // Do something with the found objects
+                
+                for object in objects {
+                    var info: Dictionary<String, AnyObject> = Dictionary<String, AnyObject>()
+                    info["companyName"] = object["company"] as String!
+                    info["room"] = object["room"] as String!
+                    info["speaker"] = object["speaker"] as String!
+                    info["description"] = object["description"] as String!
+                    info["startTime"] = object["startTime"] as NSDate!
+                    info["endTime"] = object["endTime"] as NSDate!
+                    info["email"] = object["email"] as String!
+                    info["companyWebsite"] = NSURL(string: object["companyWebsite"] as String!)!
+                    info["twitterHandle"] = object["twitterHandle"] as String!
+                    info["image"] = object["image"] as PFFile!
+                    
+                    var event: Event = Event(info: info)
+                    self.events.append(event)
+                }
+                
+                self.events = self.events.sorted({
+                    (firstEvent: Event, secondEvent: Event) -> Bool in
+                    return firstEvent.startTime.description < secondEvent.startTime.description
+                })
+                
+                for event in self.events {
+                    println("Time: \(event.startTime)")
+                }
+                
+                self.tableView.reloadData()
+                
+            } else {
+                // Log details of the failure
+                println("Error: %@ %@", error, error.userInfo!)
+            }
+        }
+    }
+    
+     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
 
-    required init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+     required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return sectionNum
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 75.00
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "\(section):00";
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
     }
     
 }
