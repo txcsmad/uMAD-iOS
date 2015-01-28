9e89f41ac9eea74f9392c56456cd39cde9227750
schun <s.chun@utexas.edu>
2015-01-28 16:34:03 -0600

    Finished parsing information and sorted times

--- uMAD/AppDelegate.swift
+++ uMAD/AppDelegate.swift
@@ -1,6 +1,9 @@
 
 import UIKit
 
+let PARSE_APPLICATION_ID: String = "uY4oviE7S1f5tJ4naI4J0BExh6qSTYUwdQCpukoX"
+let PARSE_CLIENT_KEY: String = "Y6X3s9CzWvduMX3P9oTB0mZBEphF9ntdKlj1HEU9"
+
 @UIApplicationMain
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -8,7 +11,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        Parse.setApplicationId("uY4oviE7S1f5tJ4naI4J0BExh6qSTYUwdQCpukoX", clientKey: "Y6X3s9CzWvduMX3P9oTB0mZBEphF9ntdKlj1HEU9")
+        Parse.setApplicationId(PARSE_APPLICATION_ID, clientKey: PARSE_CLIENT_KEY)
         
         var eventsViewController = UINavigationController(rootViewController: EventsViewController())
         var twitterViewController = UINavigationController(rootViewController: TwitterViewController())
--- uMAD/uMAD-Bridging-Header.h
+++ uMAD/uMAD-Bridging-Header.h
@@ -5,3 +5,4 @@
 @import Parse;
 
 #import <STTwitter/STTwitter.h>
+#import "UIImage+Resize.h"
