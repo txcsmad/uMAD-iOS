bef77e83bc7c8478b2f10c1e2b733870f8366f5d
Jesse <tiptonjesse@gmail.com>
2015-01-28 17:37:00 -0600

    Changes to Twitter

--- uMAD/TwitterViewController.swift
+++ uMAD/TwitterViewController.swift
@@ -4,35 +4,23 @@ import UIKit
 let TWITTER_CONSUMER_KEY = "SyuNTOUuYd5EbejRrzTXoNys3"
 let TWITTER_CONSUMER_SECRET = "6TzSt7ruEZCUElzui3U98UxovXm88ZiByPL3ARMBjkUbkIgdv9"
 
-struct Tweet {
-    
-    var createdAt: NSDate
-    var favoriteCount: Int
-    var text: String
-    var retweetCount: Int
-    var user: User
-    
-}
-
-struct User {
-    
-    var name: String
-    var screenName: String
-    var profileImageURL: NSURL
-    
-}
+let UTCS_MAD = "utcsmad"
+let TWEET_BATCH_COUNT: UInt = 100
 
 class TwitterViewController: UITableViewController {
     
     let twitter = STTwitterAPI(appOnlyWithConsumerKey: TWITTER_CONSUMER_KEY, consumerSecret: TWITTER_CONSUMER_SECRET)
     var tweets = [NSDictionary]()
     
+    func composeTweet() {
+        println("Compose tweet")
+    }
+    
     func reloadTweets() {
-        twitter.getSearchTweetsWithQuery("#dope", geocode: nil, lang: nil, locale: nil, resultType: nil, count: "200", until: nil, sinceID: nil, maxID: nil, includeEntities: nil, callback: nil, successBlock: { (_, response: [AnyObject]!) in
+        twitter.getUserTimelineWithScreenName(UTCS_MAD, count: TWEET_BATCH_COUNT, successBlock: { (response: [AnyObject]!) in
             self.tweets = response as [NSDictionary]
-            
             self.tableView.reloadData()
-        }, errorBlock: { (error: NSError!) in
+        }, { (error: NSError!) in
             UIAlertController.presentErrorAlertControllerInViewController(self, error: error)
         })
     }
@@ -42,11 +30,18 @@ class TwitterViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.registerClass(TweetTableViewCell.classForCoder(), forCellReuseIdentifier: "TweetCell")
+        navigationController?.tabBarItem.title = "Twitter"
+        
+        navigationItem.title = "Twitter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Compose, target: self, action: Selector("composeTweet"))
+        
+        tableView.registerNib(UINib(nibName: "TweetTableViewCell", bundle: NSBundle.mainBundle()), forCellReuseIdentifier: "TweetCell")
+        // tableView.registerClass(TweetTableViewCell.classForCoder(), forCellReuseIdentifier: "TweetCell")
         
         twitter.verifyCredentialsWithSuccessBlock({ (bearerToken: String!) in
             self.reloadTweets()
         }, errorBlock: { (error: NSError!) in
+            println("couldn't verify credentials")
             UIAlertController.presentErrorAlertControllerInViewController(self, error: error)
         })
     }
@@ -58,7 +53,7 @@ class TwitterViewController: UITableViewController {
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("TweetCell", forIndexPath: indexPath) as TweetTableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("TweetCell", forIndexPath: indexPath) as UITableViewCell
         
         let tweet = tweets[indexPath.row]
         let user = tweet["user"] as NSDictionary
@@ -78,21 +73,13 @@ class TwitterViewController: UITableViewController {
 
 class TweetTableViewCell: UITableViewCell {
     
-    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
-        super.init(style: .Subtitle, reuseIdentifier: reuseIdentifier)
-    }
-
-    required init(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
 }
 
 extension UIAlertController {
     
     class func presentErrorAlertControllerInViewController(viewController: UIViewController, error: NSError) {
         let alertController = UIAlertController(title: "Oops!", message: error.localizedDescription, preferredStyle: .Alert)
-        alertController.addAction(UIAlertAction(title: "Okay", style: .Default, handler: nil))
+        alertController.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
         viewController.presentViewController(alertController, animated: true, completion: nil)
     }
     
