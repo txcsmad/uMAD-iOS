ddcc66d8312c9e12f14e5122bd435b359611e289
Jesse <tiptonjesse@gmail.com>
2015-01-29 19:44:07 -0600

    Finished Twitter view controller

---
+++ uMAD/TweetTableViewCell.swift
@@ -0,0 +1,19 @@
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+    
+    @IBOutlet var userProfileImageView: UIImageView!
+    @IBOutlet var userNameAndScreenNameLabel: UILabel!
+    @IBOutlet var createdAtLabel: UILabel!
+    @IBOutlet var tweetTextLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        userProfileImageView.backgroundColor = UIColor.lightGrayColor()
+        userProfileImageView.layer.cornerRadius = 8
+        userProfileImageView.clipsToBounds = true
+    }
+    
+}
