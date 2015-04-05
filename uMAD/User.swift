eab00b29083650762fd95eddf35d34b5f16c3d86
Nick Walker <nick@nickwalker.us>
2015-04-04 23:03:09 -0500

    Begin refactoring tweet storage
    Move management logic into a singleton
    Break models into separate files

---
+++ uMAD/User.swift
@@ -0,0 +1,27 @@
+struct User {
+
+    let id: String
+    let name: String
+    let screenName: String
+    let profileImageUrl: NSURL
+
+    private init(json: NSDictionary) {
+        id = json["id_str"] as! String
+        name = json["name"] as! String
+        screenName = json["screen_name"] as! String
+
+        let profileImageUrlString = json["profile_image_url_https"] as! String
+        profileImageUrl = NSURL(string: profileImageUrlString)!
+    }
+
+    static func getUser(json: NSDictionary) -> User {
+        let cached = TimelineManager.instance.users[json["name"] as! String]
+        if cached != nil {
+            return cached!
+        }
+        else {
+            return User(json: json)
+        }
+    }
+    
+}
