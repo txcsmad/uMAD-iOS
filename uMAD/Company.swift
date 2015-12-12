5f05ba8387d2dd04cffc6b5a921a00a6240fbb81
Jesse Tipton <me@jessetipton.com>
2015-12-11 19:12:04 -0600

    cleaned up company model
    * removed `get` wrapper from get-only computed property, as is convention
    * removed unnecessary `objectID` property
    * removed unused function `stringDescription()`

--- uMAD/Company.swift
+++ uMAD/Company.swift
@@ -1,28 +1,20 @@
+
 import Parse
 
-
 class Company: PFObject, PFSubclassing {
+    
     @NSManaged var name: String
     @NSManaged var sponsorLevel: Int
     @NSManaged var website: String
     @NSManaged var twitterHandle: String
-    @NSManaged var objectID: String
     @NSManaged var image: PFFile
     @NSManaged var thumbnail: PFFile
-
-    var websiteURL:NSURL {
-        get{
-            return NSURL(string: website)!
-        }
+    var websiteURL: NSURL {
+        return NSURL(string: website)!
     }
     
     static func parseClassName() -> String {
         return "Company"
     }
-   func stringDescription() -> String {
-        return "\(name)"
-    }
-
-
-
+    
 }
