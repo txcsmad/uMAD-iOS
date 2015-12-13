d8df5014da1e83b904858680cf54af5e4d12f78f
Jesse Tipton <me@jessetipton.com>
2015-12-12 20:49:21 -0600

    updated config template

--- uMAD/Config.template.swift
+++ uMAD/Config.template.swift
@@ -1,10 +1,14 @@
+
+import UIKit
+
 struct Config {
-    //Get this stuff from Twitter
-    static let twitterConsumerKey = ""
-    static let twitterConsumerSecret = ""
+    // Get this stuff from Twitter
     static let twitterHandle = "@yourtwitterhandle"
 
-    //Create these in the Parse dashboard
+    // Create these in the Parse dashboard
     static let parseAppID = ""
     static let parseClientKey = ""
+    
+    // Tint color
+    static let tintColor = UIColor.blueColor()
 }
