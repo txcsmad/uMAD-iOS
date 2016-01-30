b77e6938bb834da6246b544c2ca5389cfe9b4f30
Nick Walker <nick@nickwalker.us>
2016-01-30 13:08:08 -0600

    Make sign in trigger on keyboard return
    Remove copyright header
    Remove sign in nav bar action

--- uMAD/LogInViewController.swift
+++ uMAD/LogInViewController.swift
@@ -1,11 +1,3 @@
-//
-//  Log-InViewController.swift
-//  uMAD
-//
-//  Created by Mark Malstrom on 1/26/16.
-//  Copyright © 2016 com.MAD. All rights reserved.
-//
-
 import UIKit
 import SafariServices
 import Parse
@@ -15,47 +7,80 @@ protocol LogInViewControllerDelegate: class {
 }
 
 class LogInViewController: UITableViewController, UITextFieldDelegate {
-	// MARK: -
 	@IBOutlet weak var usernameTextField: UITextField!
 	@IBOutlet weak var passwordTextField: UITextField!
-	@IBOutlet weak var signInButton: UIBarButtonItem!
 
 	weak var delegate: LogInViewControllerDelegate?
 
-	let signUpFormURL = NSURL(string: "http://www.cs.utexas.edu/users/mad/")!
+	let signUpFormURL = NSURL(string: "http://umad.me")!
 
-	// MARK: -
+    enum Section: Int {
+        case Fields
+        case Actions
+    }
+
+	// MARK:- Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-		signInButton.enabled = false
 		usernameTextField.becomeFirstResponder()
 		usernameTextField.delegate = self
 		passwordTextField.delegate = self
 	}
 
 	// Called when the "Sign Up" button is pressed.
-	func signUp() {
+	private func signUp() {
 		let signUpController = SFSafariViewController(URL: signUpFormURL)
 		signUpController.view.tintColor = Config.tintColor
 		presentViewController(signUpController, animated: true, completion: nil)
 	}
 
-	func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
-		signInButton.enabled = !passwordTextField.text!.isEmpty && !usernameTextField.text!.isEmpty
-		return true
-	}
+    private func signIn() {
+        guard let username = usernameTextField.text,
+            password = passwordTextField.text else {
+                return
+        }
 
-	// MARK: - Table View Methods
+        PFUser.logInWithUsernameInBackground(username, password: password) { user, error in
+            if let _ = user {
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.delegate?.logInViewController()
+                }
+                return
+            }
+
+            let alert = UIAlertController(title: "Error",
+                message: "\(error?.localizedDescription ?? "An error occurred")", preferredStyle: .Alert)
+            let OKAction = UIAlertAction(title: "Okay", style: .Default, handler: nil)
+            alert.addAction(OKAction)
+            alert.view.tintColor = Config.tintColor
+            self.presentViewController(alert, animated: true, completion: nil)
+
+        }
+    }
+
+    // MARK:- TextField Delegate
+
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        signIn()
+        return true
+    }
+
+	// MARK:- Table View Methods
 	override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-		if indexPath == NSIndexPath(forRow: 1, inSection: 1) {
+        guard let section = Section(rawValue: indexPath.section) else {
+            return
+        }
+		if section == .Actions && indexPath.row == 1 {
 			signUp()
-		}
-		
+        }
 		tableView.deselectRowAtIndexPath(indexPath, animated: true)
 	}
 
 	override func tableView(tableView: UITableView, shouldHighlightRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-		return indexPath.section == 1 && indexPath.row < 2
+        guard let section = Section(rawValue: indexPath.section) else {
+            return false
+        }
+		return section == .Actions
 	}
 
 	// MARK: - Bar Button Item IBActions
@@ -64,22 +89,4 @@ class LogInViewController: UITableViewController, UITextFieldDelegate {
 		self.dismissViewControllerAnimated(true, completion: nil)
 	}
 
-	@IBAction func signIn(sender: UIBarButtonItem) {
-		let username: String! = usernameTextField.text
-		let password: String! = passwordTextField.text
-
-		PFUser.logInWithUsernameInBackground(username, password: password) { (user, error) -> Void in
-			if let _ = user {
-				dispatch_async(dispatch_get_main_queue(), { () -> Void in
-					self.delegate?.logInViewController()
-				})
-			} else {
-				let alert = UIAlertController(title: "Error", message: "\(error)", preferredStyle: .Alert)
-				let OKAction = UIAlertAction(title: "Okay", style: .Default, handler: nil)
-				alert.addAction(OKAction)
-				alert.view.tintColor = Config.tintColor
-				self.presentViewController(alert, animated: true, completion: nil)
-			}
-		}
-	}
 }
